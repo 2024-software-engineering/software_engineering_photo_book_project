4df4b1e499c15e861a982c67d6f90770e8dd5bcb
박성재 <sem201@naver.com>
2024-06-06 02:33:00 +0900

    사진 업로드 및 메인페이지에 출력

--- signup.py
+++ signup.py
@@ -1,10 +1,15 @@
 import sqlite3
 from flask import Flask,redirect, url_for, render_template,request,session
 
+
 app = Flask(__name__)
 app.secret_key = '1234'
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
-def mainpage():
+def loginpage():
     return render_template('login_page.html')
 
 @app.route('/signup_page')
@@ -35,7 +40,6 @@ def create_user():
 @app.route('/login',methods=['POST','GET'])
 def check_user():
     if request.method=='POST':
-            
             email=request.form['email']
             password= request.form['password']
 
@@ -46,9 +50,10 @@ def check_user():
 
                 if user:
                     session['user_id'] =user[0]
-                    return render_template("mainpage.html")
+                    session['user_nickname'] = user[1]
+                    return redirect(url_for('mainpage'))
                 else:
-                    return render_template("login_page.html")
+                    return render_template("loginpage.html")
 
 #사진 디테일 페이지로 리다이렉트
 @app.route('/photo_detail/<int:item_id>')
@@ -62,13 +67,20 @@ def photo_detail(item_id):
             WHERE DM_table.photo_ID = ?
         ''', (item_id,))
         dms = cur.fetchall()
-    
+    cur.execute('''
+            SELECT photo_img, user_table.user_nickname, photo_describ
+            FROM photo_table
+            JOIN user_table ON photo_table.user_ID = user_table.ID
+            WHERE photo_ID = ?
+        ''', (item_id,))
+    photo = cur.fetchone()
+
     item = {
         'id': item_id,
-        'author': 'woo',
+        'author': photo[1],
         'keywords': '#키워드',
-        'description': '어쩌구저쩌구 설명~~~~',
-        'img_src': 'assets/dummy_img.png'
+        'description': photo[2],
+        'img_src': 'photo[0]'
     }
     return render_template('photo_detail.html', item=item, dms=dms)
 
@@ -78,6 +90,7 @@ def msg_send():
     
     user_id = session['user_id']
     dm_msg = request.form['msg']
+    photo_id=request.form['photo_id']
 
     with sqlite3.connect('photo_album.db') as con:
         cur = con.cursor()
@@ -103,6 +116,72 @@ def delete_dm(dm_id):
 
     photo_id = request.form['photo_id']
     return redirect(url_for('photo_detail', item_id=photo_id))
+
+#사진 upload
+
+
+#업로드 페이지로 렌더링
+@app.route('/upload')
+def photo_upload():
+    return render_template('photo_upload.html')
+    
+
+@app.route('/mainpage')
+def mainpage():
+    with sqlite3.connect('photo_album.db') as con:
+        cur = con.cursor()
+        cur.execute('''
+            SELECT photo_table.photo_ID, photo_table.photo_img, user_table.user_nickname, 
+                GROUP_CONCAT(photo_keyword_table.keyword), photo_table.photo_describ
+            FROM photo_table
+            JOIN user_table ON photo_table.user_ID = user_table.ID
+            LEFT JOIN photo_keyword_table ON photo_table.photo_ID = photo_keyword_table.photo_ID
+            GROUP BY photo_table.photo_ID
+        ''')
+        photos = cur.fetchall()
+
+    return render_template('mainpage.html', photos=photos)
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'user_id' not in session:
+        return redirect(url_for('loginpage'))
+
+    user_id = session['user_id']
+    photo = request.files['photo']
+    keyword = request.form['key']
+    description = request.form['des']
+
+    # 사진을 저장할 경로 설정
+    photo_path = f'static/uploads/{photo.filename}'
+    photo.save(photo_path)
+    photo_path= f'uploads/{photo.filename}'
+    with sqlite3.connect('photo_album.db') as con:
+        cur = con.cursor()
+
+        # photo_table에 데이터 삽입
+        cur.execute('''
+            INSERT INTO photo_table (user_ID, photo_img, photo_describ)
+            VALUES (?, ?, ?)
+        ''', (user_id, photo_path, description))
+
+        # 삽입된 photo ID 가져오기
+        photo_id = cur.lastrowid
+
+        # photo_keyword_table에 데이터 삽입
+        cur.execute('''
+            INSERT INTO photo_keyword_table (photo_ID, keyword)
+            VALUES (?, ?)
+        ''', (photo_id, keyword))
+
+        con.commit()
+
+    return redirect(url_for('loginpage'))
+
+@app.route('/message')
+def dm_list():
+    return render_template('dm_list.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
